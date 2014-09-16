d4b568186ef1397eb5d5b7862bdb9d8d94cb47c7
Anesh Patel <aneshp92@gmail.com>
2014-09-16 17:27:23 -0500

    Updated

--- census_app/server.R
+++ census_app/server.R
@@ -11,8 +11,8 @@ shinyServer(
   function(input, output) {
     output$map <- renderPlot({
       args <- switch(input$var,
-                     "Submitted Via" = list(cc$white, "darkgreen", "% White"),
-                     "ZipCode" = list(cc$black, "black", "% Black"),
+                     "Submitted Via" = list(counties$white, "darkgreen", "% White"),
+                     "ZipCode" = list(counties$black, "black", "% Black"),
       args$min <- input$range[1]
       args$max <- input$range[2]
       
